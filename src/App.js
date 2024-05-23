cf4ddfb2d3876cfdb4171dc245b2c2337b7eaa5a
minnoiii <shmin1202@gmail.com>
2024-05-23 13:00:23 +0900

    last

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { shopInfo } from './data.js';
+
+function Card(props) {
+  return (
+    <>
+    <div className='col-md-4'>
+      <img src={"https://codingapple1.github.io/shop/shoes" + (props.i + 1) + ".jpg"} width="80%"></img>
+      <h4>{props.shoes[props.i].title}</h4>
+      <p>{props.shoes[props.i].price}</p>
+    </div>
+    </>
+  );
+}
 
 function App() {
+  const [shoes] = useState(shopInfo);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="light" data-bs-theme="light">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+      <div className="main-bg"></div>
+      
+      <div className='container'>
+        <div className='row'>
+          {shoes.map((a,i) => {
+            return (
+              <Card shoes={shoes} i={i}></Card>
+            )
+          })}
+        </div>
+      </div>
     </div>
   );
 }
